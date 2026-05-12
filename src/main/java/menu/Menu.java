23cf5cd7fd5970db94fb7d2510baeef728769d94
KevinOSull <kosullivan3336@gmail.com>
2026-05-13 00:41:06 +0100

    Added properties to constructor,added getitle and getselectedindex

--- src/main/java/menu/Menu.java
+++ src/main/java/menu/Menu.java
@@ -1,13 +1,29 @@
 package menu;
 
+import java.util.Arrays;
+
 public class Menu {
 
     private String title = "";
-    private String[] items = {""};
+    private String[] items = new String[0];
     private int selectedIndex;
 
-    public Menu(){
+    public Menu(String title,String[]items,int selectedIndex){
+        this.title = title;
+        this.items = Arrays.copyOf(items,items.length);
+        this.selectedIndex = selectedIndex;
+    }
 
+    public String[] getItems(){
+        return this.items;
+    }
+
+    public String getTitle(){
+        return this.title;
+    }
+
+    public int getSelectedIndex(){
+        return this.selectedIndex;
     }
 
     private void moveUp(){
