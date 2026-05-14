849e20fab094ee5815007cbfc95896d5b5404a4a
KevinOSull <kosullivan3336@gmail.com>
2026-05-14 23:09:14 +0100

    Added methods to control screen state

--- src/main/java/menu/MenuManager.java
+++ src/main/java/menu/MenuManager.java
@@ -18,7 +18,6 @@ public class MenuManager {
         mainMenu = new Menu("Main Menu",new String[]{"Choose Preset Size","Set Custom Size","Back to Main"},0);
         setGridSizeMenu = new Menu("Set Grid Size",new String[]{"10 x 10","15 x 15","20 x 20","30 x 30","40 x 40","50 x 50"},0);
         setGameDifficultyMenu = new Menu("Very Easy",new String[]{"Easy","Medium","Hard","Very Hard","Back"},0);
-
     }
 
     public Menu getMenu(ScreenState screenState){
@@ -34,7 +33,48 @@ public class MenuManager {
         throw new IllegalStateException("Unknown screen state");
     }
 
-    public ScreenState handleSelection(Menu menu){
-        return null;
+    public ScreenState handleSelection(Menu menu,ScreenState screenState){
+        switch(screenState){
+            case START_MENU:
+            return handleStartMenuIndex(menu.getSelectedIndex());
+
+            case MAIN_MENU_ITEMS_LIST:
+                return handleMainMenuIndex(menu.getSelectedIndex());
+
+            default:
+                throw new IllegalStateException("UNKNOWN SCREEN STATE:! " + screenState);
+
+        }
     }
+
+    public ScreenState handleStartMenuIndex(int index){
+        switch(index){
+            case 0:
+                return ScreenState.MAIN_MENU_ITEMS_LIST;
+
+            case 1:
+                return ScreenState.EXIT_GAME;
+
+            default:
+                throw new IllegalStateException("INVALID START_MENU SELECTION!: " + index);
+        }
+    }
+
+    public ScreenState handleMainMenuIndex(int index){
+        switch(index){
+            case 0:
+                return ScreenState.SET_GRID_SIZE_MENU;
+
+            case 1:
+                return ScreenState.USER_SET_GRID_SIZE;
+
+            case 2:
+                return ScreenState.START_MENU;
+
+            default:
+                throw new IllegalStateException("INVALID MAIN MENU SELECTION!: " + index);
+        }
+    }
+
+
 }
